1360c7f8b0a12a3a40efad59de0904440365bb9f
hvzy <hadimirza1@gmail.com>
2020-12-21 20:30:13 -0500

    fix to get individual name from URL id

--- controllers/list.js
+++ controllers/list.js
@@ -1,21 +1,21 @@
-let importList = require('../models/list.js')
+const importList = require("../models/list.js");
 
 function index(req, res, next) {
-  res.render('list.ejs', {importList});
-};
-
-
-// This is the part I can't figure out. Why can't I display the forEach values of 'name'? I'm pretty sure my function in models/list.js is ok
-
-function item(req,res,next) {
-    // res.send(req.params.id)
-    // res.send(importList.getEach())    .......... This is the forEach function, I can't call it because I messed up somewhere
-    res.render('single-item', {importList})  // Using render works but I cant call importList.products within the .ejs either
+  res.render("list.ejs", { importList });
 }
 
-//
+function item(req, res, next) {
+  const {
+    params: { id: itemId },
+  } = req;
+  // const {id} = params
+  // params = '5' // immutable
+  // importList = [1,2,3,4,5]
+  // console.log(req);
+  res.render("single-item", { names: importList.products, id: itemId });
+}
 
 module.exports = {
-    index,
-    item,
-}
+  index,
+  item,
+};
